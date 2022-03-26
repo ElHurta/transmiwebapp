3a46f401f54808c3a2dd02a13b2b5f9d28475000
ElHurta <juan.david.hurtado08@gmail.com>
2022-03-25 19:04:23 -0500

    DAO Client general query completed

--- src/main/java/com/hurtado/transmiwebapp/clientsServlet.java
+++ src/main/java/com/hurtado/transmiwebapp/clientsServlet.java
@@ -1,15 +1,36 @@
 package com.hurtado.transmiwebapp;
 
+import data.DAO.ClienteDAO;
+import model.Cliente;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "clientsServlet", value = "/clientsServlet")
 public class clientsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession misession = request.getSession();
+
+        if(misession.getAttribute("Logged") != null){
+            if(misession.getAttribute("Logged").equals("true")){
+                ClienteDAO clienteDAO = new ClienteDAO();
+                ArrayList<Cliente> clientesList = clienteDAO.queryAllClients();
+
+                request.setAttribute("clientesList", clientesList);
+                RequestDispatcher rd = request.getRequestDispatcher("/clientes.jsp");
+
+                rd.forward(request, response);
+            } else{
+                response.sendRedirect("/mainView.jsp");
+            }
+        } else{
+            response.sendRedirect("/mainView.jsp");
+        }
     }
 
     @Override
--- src/main/java/data/DAO/ClienteDAO.java
+++ src/main/java/data/DAO/ClienteDAO.java
@@ -1,4 +1,30 @@
 package data.DAO;
 
+import data.Operaciones;
+import model.Cliente;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ClienteDAO {
+    Operaciones op;
+
+    public ClienteDAO() {op = new Operaciones();}
+
+    public ArrayList<Cliente> queryAllClients(){
+        ArrayList<Cliente> clientsArray = new ArrayList<>();
+        try {
+            ResultSet resultSet = op.ConsultaEsp("SELECT * FROM Cliente");
+            while(resultSet.next()){
+                clientsArray.add(new Cliente(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3)));
+            }
+
+        } catch (SQLException ex){
+            System.out.println(ex);
+        }
+        return clientsArray;
+    }
 }
